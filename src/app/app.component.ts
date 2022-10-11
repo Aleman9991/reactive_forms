2a4a115cbdb74dde00795766250c3d8a6ff17044
Aleman9991 <jealeman1@hotmail.com>
2022-10-10 21:13:10 -0500

    ticket_turno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'reactive_forms';
+  formulario = new FormGroup({
+    nombre_completo: new FormControl('',Validators.required),
+    curp: new FormControl('',Validators.required),
+    nombre: new FormControl('',Validators.required),
+    paterno: new FormControl('',Validators.required),
+    materno: new FormControl('',Validators.required),
+    telefono: new FormControl('',Validators.required),
+    celular: new FormControl('',Validators.required),
+    correo: new FormControl('',Validators.required)
+    
+  });
+  
 }
